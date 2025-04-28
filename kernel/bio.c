c59a36c51a6d4e6596914b71acc5cc034d258e1c
masterAllen <allenhong2000@foxmail.com>
2025-04-28 19:06:17 +0800

    solve q2 bcache

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,7 +23,7 @@
 #include "fs.h"
 #include "buf.h"
 
-#define HASH_SIZE 31
+#define HASH_SIZE 13
 
 
 struct {
@@ -44,7 +44,7 @@ binit(void)
     snprintf(name, 10, "bcache%d", i);
     initlock(&bcache.lock[i], name);
 
-    struct buf *head = &bcache.head[idx];
+    struct buf *head = &bcache.head[i];
     head->next = head;
     head->prev = head;
   }
@@ -61,9 +61,6 @@ binit(void)
     head->next = now;
 
     initsleeplock(&now->lock, "buffer");
-
-    printf("head->next = %p\n", head->next);
-    printf("head->prev = %p\n", head->prev);
   }
 }
 
@@ -94,8 +91,6 @@ bget(uint dev, uint blockno)
       return b;
     }
   }
-  release(&bcache.lock[idx]);
-
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
@@ -110,34 +105,83 @@ bget(uint dev, uint blockno)
       return b;
     }
   }
+  release(&bcache.lock[idx]);
+
+  // 假如 release
+  // 1. bget(0) myself: acquire-release, 没找到, 准备开始大锁
+  // 2. brelse(0) other: acquire-release, OK...
+  // 3. bget(0) other: acquire-release, 找到一个空的
+  // 4. bget(0) myself: 大锁之后，从别的地方拿过来一个
+  // 5. 出错，有两个了..
+
+  // 假如 release，但是大锁之后会检测
+  // 那也不对，第 4 步中如果发生在检测之后，还是会从别的地方拿一个
+
+  // 假如不 release: 死锁
+
+  // 假如 release，但是大锁最后要改变的时候才会检测？
+  // 似乎可以 --> 没错，就是可以
 
   // 本次哈希没有，所以要从其他桶中借一个过来
   uint min_ticks = 0xFFFF;
   int min_idx = idx;
-  struct buf *nowb;
+  struct buf *min_b;
   for (int i = 0; i < HASH_SIZE; ++i) {
     if (i == idx) continue;
 
     acquire(&bcache.lock[i]);
     head = &(bcache.head[i]);
-    for(nowb = head->prev; nowb != head; nowb = nowb->prev){
+    for(b = head->prev; b != head; b = b->prev){
       // 因为每个桶自己也维护了顺序，所以只要找到一个，就是当前桶的 LRU 了
-      if(nowb->refcnt == 0 && nowb->ticks < min_ticks) {
-        min_ticks = nowb->ticks;
+      if(b->refcnt == 0 && b->ticks < min_ticks) {
+        min_ticks = b->ticks;
         min_idx = i;
-        b = nowb;
+        min_b = b;
         break;
       }
     }
     release(&bcache.lock[i]);
-
   }
+
   if (min_ticks != 0xFFFF) {
+    // 要开始改变了，原则：不要同时拿两个锁
+
+    // 有一个规定：
+    // 先强制删掉别人的块，即使后面发现不需要了，也还是放在我们这里不归还
+    // 其实可以归还，只不过代码改一下就好了
+
+    // 删掉别人的块，等会我们可能用它 (unlink)
+    acquire(&bcache.lock[min_idx]);
+    min_b->next->prev = min_b->prev;
+    min_b->prev->next = min_b->next;
+    release(&bcache.lock[min_idx]);
+
+    acquire(&bcache.lock[idx]);
+    head = &bcache.head[idx];
+
+    // 不管最后要不要别人的块，这个块放在这里，并且放在最后一个中
+    min_b->prev = head->prev;
+    head->prev->next = min_b;
+    head->prev = min_b;
+    min_b->next = head;
+
+    // 需要先检测目标是否已经分配了
+    for(b = head->next; b != head; b = b->next){
+      if(b->dev == dev && b->blockno == blockno){
+        b->refcnt++;
+        release(&bcache.lock[idx]);
+        acquiresleep(&b->lock);
+        return b;
+      }
+    }
+
+    // 没有分配，就用这个块
+    b = min_b;
     b->dev = dev;
     b->blockno = blockno;
     b->valid = 0;
     b->refcnt = 1;
-    release(&bcache.lock[min_idx]);
+    release(&bcache.lock[idx]);
     acquiresleep(&b->lock);
     return b;
   } 
@@ -197,16 +241,18 @@ brelse(struct buf *b)
 
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
+  int idx = bhash(b->dev, b->blockno);
+  acquire(&bcache.lock[idx]);
   b->refcnt++;
-  release(&bcache.lock);
+  release(&bcache.lock[idx]);
 }
 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
+  int idx = bhash(b->dev, b->blockno);
+  acquire(&bcache.lock[idx]);
   b->refcnt--;
-  release(&bcache.lock);
+  release(&bcache.lock[idx]);
 }
 
 
